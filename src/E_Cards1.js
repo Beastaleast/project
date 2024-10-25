5311aa20a144107f2355664fac86209b07f8f7b8
Udit <uditkushwaha10107@gamil.com>
2024-10-25 11:30:56 +0530

    Added

--- src/E_Cards1.js
+++ src/E_Cards1.js
@@ -1,20 +1,15 @@
 import React from "react";
 
-
-
 function E_Cards1(props) {
   console.log(props);
   return (
     <div className="container">
- 
       <div className="u">
         <img src={props.img} alt="" />
         <h2>Text</h2>
         <p>$0</p>
         <p>Body text.</p>
       </div>
-      
-
     </div>
   );
 }
