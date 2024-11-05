b05768cdd0600bb913d36dd38003baae2e47f062
Udit <uditkushwaha10107@gamil.com>
2024-11-05 17:18:18 +0530

    Added

--- src/ClientDashboard/Cle.js
+++ src/ClientDashboard/Cle.js
@@ -1,76 +1,116 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Cle.css";
+import axios from "axios";
 
-const initialClients = [
-  {
-    id: 1,
-    name: "Mr. Ankit Shakya",
-    phone: "+91-6387780207",
-    email: "shakya738@gmail.com",
-    nutritionist: "Azra Khan",
-    tags: "----",
-    service: "Comprehensive Weight Loss Plan",
-    planStart: "Aug 6, 2024",
-    planEnd: "Feb 2, 2025",
-    status: "Active",
-  },
-  {
-    id: 2,
-    name: "Mr. Despendra Shakya",
-    phone: "+91-9791821922",
-    email: "shakyaNeha548@gmail.com",
-    nutritionist: "Neha Shakya",
-    tags: "----",
-    service: "Ultimate Weight Loss Plan",
-    planStart: "Jul 2, 2024",
-    planEnd: "Sep 30, 2024",
-    status: "Active",
-  },
-  {
-    id: 3,
-    name: "Mr. Despendra Shakya",
-    phone: "+91-9791821922",
-    email: "shakyaNeha548@gmail.com",
-    nutritionist: "Neha Shakya",
-    tags: "----",
-    service: "Ultimate Weight Loss Plan",
-    planStart: "Jul 2, 2024",
-    planEnd: "Sep 30, 2024",
-    status: "Active",
-  },
-  {
-    id: 5,
-    name: "Mr. Despendra Shakya",
-    phone: "+91-9791821922",
-    email: "shakyaNeha548@gmail.com",
-    nutritionist: "Neha Shakya",
-    tags: "----",
-    service: "Ultimate Weight Loss Plan",
-    planStart: "Jul 2, 2024",
-    planEnd: "Sep 30, 2024",
-    status: "Active",
-  },
-  {
-    id: 2,
-    name: "Mr. Despendra Shakya",
-    phone: "+91-9791821922",
-    email: "shakyaNeha548@gmail.com",
-    nutritionist: "Neha Shakya",
-    tags: "----",
-    service: "Ultimate Weight Loss Plan",
-    planStart: "Jul 2, 2024",
-    planEnd: "Sep 30, 2024",
-    status: "Active",
-  },
-];
+// const initialClients = [
+//   {
+//     id: 1,
+//     name: "Mr. Ankit Shakya",
+//     phone: "+91-6387780207",
+//     email: "shakya738@gmail.com",
+//     nutritionist: "Azra Khan",
+//     tags: "----",
+//     service: "Comprehensive Weight Loss Plan",
+//     planStart: "Aug 6, 2024",
+//     planEnd: "Feb 2, 2025",
+//     status: "Active",
+//   },
+//   {
+//     id: 2,
+//     name: "Mr. Despendra Shakya",
+//     phone: "+91-9791821922",
+//     email: "shakyaNeha548@gmail.com",
+//     nutritionist: "Neha Shakya",
+//     tags: "----",
+//     service: "Ultimate Weight Loss Plan",
+//     planStart: "Jul 2, 2024",
+//     planEnd: "Sep 30, 2024",
+//     status: "Active",
+//   },
+//   {
+//     id: 3,
+//     name: "Mr. Despendra Shakya",
+//     phone: "+91-9791821922",
+//     email: "shakyaNeha548@gmail.com",
+//     nutritionist: "Neha Shakya",
+//     tags: "----",
+//     service: "Ultimate Weight Loss Plan",
+//     planStart: "Jul 2, 2024",
+//     planEnd: "Sep 30, 2024",
+//     status: "Active",
+//   },
+//   {
+//     id: 5,
+//     name: "Mr. Despendra Shakya",
+//     phone: "+91-9791821922",
+//     email: "shakyaNeha548@gmail.com",
+//     nutritionist: "Neha Shakya",
+//     tags: "----",
+//     service: "Ultimate Weight Loss Plan",
+//     planStart: "Jul 2, 2024",
+//     planEnd: "Sep 30, 2024",
+//     status: "Active",
+//   },
+//   {
+//     id: 2,
+//     name: "Mr. Despendra Shakya",
+//     phone: "+91-9791821922",
+//     email: "shakyaNeha548@gmail.com",
+//     nutritionist: "Neha Shakya",
+//     tags: "----",
+//     service: "Ultimate Weight Loss Plan",
+//     planStart: "Jul 2, 2024",
+//     planEnd: "Sep 30, 2024",
+//     status: "Active",
+//   },
+// ];
 
 function ClientManagement() {
+  const API = "https://my-api-rls4.vercel.app/api/getsales";
+
+  const APIDelete ="https://my-api-six-xi.vercel.app/api/delete/";
+
+  const [initialClients, setData] = useState([]);
+
+  // const res = await axios.get(API)
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    console.log(initialClients);
+  }, [initialClients]);
+
   const [clients, setClients] = useState(initialClients);
   const [searchTerm, setSearchTerm] = useState("");
 
   const deleteClient = (id) => {
-    setClients(clients.filter((client) => client.id !== id));
+    //setClients(clients.filter((client) => client.id !== id));
+//console.log(id);
+    const res = axios
+      .delete(APIDelete + id,{
+        headers: {
+          "x-api-key": "ggp-pro-ject",
+        }
+      })
+      .then(function (response) {
+        getData();
+        alert("Deleted successfully");
+      });
   };
+
+const getData =()=>
+{
+  const res = axios
+  .get(API, {
+    headers: {
+      "x-api-key": "ggp-pro-ject",
+    },
+  })
+  .then(function (response) {
+    setData(response.data);
+  });
+}
 
   const assignNutritionist = (id) => {
     const updatedClients = clients.map((client) => {
@@ -115,10 +155,11 @@ function ClientManagement() {
               <th>Plan End</th>
               <th>Status</th>
               <th>Actions</th>
+              <th>Gender</th>
             </tr>
           </thead>
           <tbody>
-            {filteredClients.map((client) => (
+            {initialClients.map((client) => (
               <tr key={client.id}>
                 <td>{client.name}</td>
                 <td>{client.phone}</td>
@@ -129,11 +170,12 @@ function ClientManagement() {
                 <td>{client.planStart}</td>
                 <td>{client.planEnd}</td>
                 <td>{client.status}</td>
+                <td>{client.gender}</td>
                 <td>
                   <button onClick={() => assignNutritionist(client.id)}>
                     Assign Nutritionist
                   </button>
-                  <button onClick={() => deleteClient(client.id)}>
+                  <button onClick={() => deleteClient(client._id)}>
                     Delete Client
                   </button>
                 </td>
