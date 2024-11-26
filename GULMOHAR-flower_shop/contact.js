c80959d76b58fb7ef6edbb41cd8625611ed84e20
Udit Kushwaha <148574038+Beastaleast@users.noreply.github.com>
2024-11-26 19:30:01 +0530

    Delete GULMOHAR-flower_shop directory

--- GULMOHAR-flower_shop/contact.js
+++
@@ -1,4 +0,0 @@
-ddocument.querySelector('.contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    alert('Thank you for contacting us. We will get back to you soon!');
-});
